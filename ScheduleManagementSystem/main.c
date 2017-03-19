cea4d2ad2ca58ca73bd323013e38e86ecbd63afd
hmnayem <hm.nayem.95@gmail.com>
2017-03-19 21:12:29 +0600

    Update some inital bug

--- ScheduleManagementSystem/main.c
+++ ScheduleManagementSystem/main.c
@@ -26,6 +26,25 @@ struct times
 typedef struct times Times;
 
 
+struct schedule
+{
+    char title[30];
+    char description[100];
+    Times entry;
+    Times expire;
+};
+
+typedef struct schedule Schedule;
+
+struct node
+{
+    Schedule task;
+    struct node *next;
+};
+
+typedef struct node List;
+
+
 // Prototyping Goes Here
 
 // All Calendar Functions Prototyping Goes Here
@@ -47,16 +66,47 @@ void calendarControl();
 // Calendar Functions Prototyping Ends Here
 
 void menus();
+void scheduleMenus();
+
+// List Functions Prototyping Starts Here
+
+
+List* createNode(Schedule task);
+void addToList(Schedule task);
+void addToDone(Schedule task);
+void removes(int index);
+void displayList();
+void displayDoneList();
+
+// List Functions Prototyping Ends Here
+
+// Schedule Operation Functions Starts Here
+
+Schedule createSchedule();
+void addSchedule();
+void upNext();
+void markDone();
+void displayDone();
+void displayNotDone();
+
+// Schedule Operation Functions End Here
+
+
+List *head;
+List *done;
 
 int currentYear;
 int currentMonth;
 int currentDay;
+int listSize = 0;
+int doneSize = 0;
 
 
 
 int main()
 {
-    int choice;
+    system("color 75");
+    int choice, subMenus;
 
     do
     {
@@ -71,6 +121,46 @@ int main()
         case 1:
             calendarControl();
             break;
+
+        case 2:
+            do
+            {
+                system("CLS");
+                scheduleMenus();
+                scanf("%d", &subMenus);
+                switch(subMenus)
+                {
+                case 0:
+                    break;
+
+                case 1:
+                    addSchedule();
+                    break;
+
+                case 2:
+                    upNext();
+                    break;
+
+                case 3:
+                    markDone();
+                    break;
+
+                case 4:
+                    displayNotDone();
+                    break;
+
+                case 5:
+                    displayDone();
+                    break;
+
+                default:
+                    printf("\n\n%40sInvalid Selections\n", " ");
+                    break;
+                }
+
+                system("pause");
+
+            }while(subMenus != 0);
 
         }
 
@@ -94,10 +184,24 @@ void menus()
     }
     printf("\n\n%20s", " ");
 
-    printf("1. Calendar%6s2. Schedule%6s3. Vacations%6s4. Help%6s0.Exit\n", " ", " ", " ", " ");
+    printf("1. Calendar%6s2. Schedule%6s3. Vacations%6s4. Help%6s0. Exit\n", " ", " ", " ", " ");
     printf("\n%50sEnter Choice : ", " ");
 
 
+}
+
+void scheduleMenus()
+{
+    printf("\n\n\n%40sSchedule Menus\n\n", " ");
+
+    printf("%40s1. Create New Schedule\n", " ");
+    printf("\n%40s2. Up Next Schedule\n", " ");
+    printf("\n%40s3. Mark As Done\n", " ");
+    printf("\n%40s4. View Active List\n", " ");
+    printf("\n%40s5. View Done List\n", " ");
+    printf("\n%40s0. Back To Menu\n", " ");
+
+    printf("\n%40sEnter Choice : ", " ");
 }
 
 // All Calendar Functions Goes Here
@@ -115,7 +219,7 @@ char* getCalendarString(Times tm)
 {
     char dates[20];
 
-    sprintf(dates, "%d, %s %d", tm.wDay, getMonthNames(tm.wMonth), tm.wYear);
+    sprintf(dates, "%d, %s %d; %d:%d", tm.wDay, getMonthNames(tm.wMonth), tm.wYear, tm.wHour, tm.wMinute);
 
     return dates;
 }
@@ -330,11 +434,305 @@ void calendarControl()
 // Calendar Functions Ends Here
 
 
+// List Functions Start Here
+
+List* createNode(Schedule task)
+{
+    List *newNode;
+    newNode = (List*)malloc(sizeof(List));
+
+    if(newNode == NULL)
+    {
+        return 0;
+    }
+
+    newNode->task = task;
+    newNode->next = NULL;
+
+    return newNode;
+
+}
+
+void addToList(Schedule task)
+{
+    char expOld[25], expNew[25];
+    List *newNode = createNode(task);
+
+    if(head == NULL)
+    {
+        head = newNode;
+        head->next = NULL;
+        listSize++;
+        return;
+    }
+
+    strcpy(expOld, getCalendarString(head->task.expire));
+    strcpy(expNew, getCalendarString(task.expire));
+
+    List *temp = head;
+
+    if(strcmp(expOld, expNew) == 1)
+    {
+        head = newNode;
+        head->next = temp;
+        listSize++;
+        return;
+    }
+
+    while(temp->next != NULL)
+    {
+        strcpy(expOld, getCalendarString(temp->next->task.expire));
+
+        if(strcmp(expOld, expNew) == 1)
+        {
+            break;
+        }
+        else
+        {
+            temp = temp->next;
+        }
+    }
+    newNode->next = temp->next;
+    temp->next = newNode;
+    listSize++;
+    return;
+}
+
+
+void addToDone(Schedule task)
+{
+    List *newNode = createNode(task);
+
+    if(done == NULL)
+    {
+        done = newNode;
+        done->next = NULL;
+        doneSize++;
+        return;
+    }
+
+    List *temp = done;
+
+    while(temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    doneSize++;
+    return;
+}
+
+void removes(int index)
+{
+    if(index < 0 || index > listSize)
+    {
+        printf("Invalid Index\n");
+        return;
+    }
+
+    if(index == 0)
+    {
+        head = head->next;
+        listSize--;
+        return;
+    }
+
+    List *temp = head;
+
+    while(index - 1 > 0)
+    {
+        temp = temp->next;
+        index--;
+    }
+
+    temp->next = temp->next->next;
+    listSize--;
+
+}
+
+void displayList()
+{
+    List *temp = head;
+    char start[25], end[25], title[30];
+    int index = 0;
+
+    printf("\n\n%20s%10s%30s%25s%25s\n", " ", "Index", "Title", "Created", "Expire");
+
+    while(temp != NULL)
+    {
+        strcpy(start, getCalendarString(temp->task.entry));
+        strcpy(end, getCalendarString(temp->task.expire));
+        strcpy(title, temp->task.title);
+
+        printf("%20s%10d%30s%25s%25s\n", " ", index++, title, start, end);
+        temp = temp->next;
+
+    }
+    printf("\n");
+
+}
+
+void displayDoneList()
+{
+    List *temp = done;
+    char start[25], end[25], title[30];
+    int index = 0;
+
+    printf("\n\n%20s%10s%30s%25s%25s\n", " ", "Index", "Title", "Created", "Expire");
+
+    while(temp != NULL)
+    {
+        strcpy(start, getCalendarString(temp->task.entry));
+        strcpy(end, getCalendarString(temp->task.expire));
+        strcpy(title, temp->task.title);
+
+        printf("%20s%10d%30s%25s%25s\n", " ", index++, title, start, end);
+        temp = temp->next;
+
+    }
+    printf("\n");
+
+}
+
+
+Schedule createSchedule()
+{
+    char title[30], description[100];
+    Times entry, expire;
+    int dd, mm, yy, hr, min;
+
+    Schedule temp;
+
+    system("CLS");
+    getchar();
+    printf("\n\n%40sCreating New Schedule\n", " ");
+
+    printf("\n%40s%10s : ", " ", "Title");
+    gets(title);
+    printf("\n%40s%10s : ", " ", "Description");
+    gets(description);
+    printf("\n%35s%15s : ", " ", "Expire Date");
+    printf("\n%35s%15s : ", " ", "(dd mm yy hh mm)");
+    scanf("%d %d %d %d %d", &dd, &mm, &yy, &hr, &min);
+
+    expire.wDay = dd;
+    expire.wMonth = mm;
+    expire.wYear = yy;
+    expire.wHour = hr;
+    expire.wMinute = mm;
+
+    entry = getCurrentTime();
+    strcpy(temp.title, title);
+    strcpy(temp.description, description);
+    temp.entry = entry;
+    temp.expire = expire;
+
+    return temp;
+
+}
+
+void addSchedule()
+{
+    Schedule temp = createSchedule();
+
+    addToList(temp);
+
+    printf("\n\n\n%35sSchedule Successfully Added\n\n\n", " ");
+}
+
+void upNext()
+{
+    char entry[25], title[30], description[100], expire[25];
+
+    system("CLS");
+    if(head == NULL)
+    {
+        printf("\n\n\n%40sThere Is No Active Task\n\n", " ");
+        return;
+    }
 
 
 
+    strcpy(entry, getCalendarString(head->task.entry));
+    strcpy(expire, getCalendarString(head->task.expire));
+    strcpy(title, head->task.title);
+    strcpy(description, head->task.description);
+
+    printf("\n\n%40sYour Next Task\n\n", " ");
+    printf("%35s%15s %s\n", " ", "Title : ", title);
+    printf("%35s%15s %s\n", " ", "Created On : ", entry);
+    printf("%35s%15s %s\n", " ", "Expire On : ", expire);
+    printf("%35s%15s %s\n", " ", "Description : ", description);
+
+    printf("\n\n");
+
+}
+
+void markDone()
+{
+    Schedule tempSchedule;
+    List *temp = head;
+
+    int index;
+
+    system("CLS");
+    printf("\n\n%40sYour Schedule List", " ");
+    printf("\n%40sEnter Index to Mark Done", " ");
+    printf("\n\n%40sEnter -1 for Cancel Editing\n", " ");
+
+    displayList();
+
+    printf("\n\n%40sEnter Index : ");
+    scanf("%d", &index);
+
+    if(index == -1)
+    {
+        printf("\n%40sCancel Editing\n");
+        return;
+    }
+
+    if(index < 0 || index > listSize)
+    {
+        printf("\n%40sInvalid Index\n", " ");
+        return;
+    }
+
+    while(index - 1 > 0)
+    {
+        temp = temp->next;
+        index--;
+    }
+
+    strcpy(tempSchedule.title, temp->task.title);
+    strcpy(tempSchedule.description, temp->task.description);
+    tempSchedule.entry = temp->task.entry;
+    tempSchedule.expire = temp->task.expire;
+
+    addToDone(tempSchedule);
+
+    removes(index);
+
+    printf("\n\n%30sSchedule Successfully Marked As Done\n", " ");
+
+}
 
 
+void displayDone()
+{
+    system("CLS");
+    printf("\n\n%40sYou Have Done This Tasks\n\n", " ");
+
+    displayDoneList();
+}
+
+
+void displayNotDone()
+{
+    system("CLS");
+
+    printf("\n\n%40sYou Have This Tasks In Queue\n\n", " ");
+    displayList();
+}
 
 
 
